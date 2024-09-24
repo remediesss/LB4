5f023bf561343d76478aa70a5055370c4b2ccfc2
remediesss <167404465+remediesss@users.noreply.github.com>
2024-09-24 08:26:02 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+
+};
+
+
+struct Node* root;
+struct Node* CreateTree(struct Node* root, struct Node* r, int data)
+{
+	if (r == NULL)
+	{
+		r = (struct Node*)malloc(sizeof(struct Node));
+		if (r == NULL)
+		{
+			printf("Ошибка выделения памяти");
+			exit(0);
+		}
+
+		r->left = NULL;
+		r->right = NULL;
+		r->data = data;
+		if (root == NULL) return r;
+
+		if (data > root->data)	root->left = r;
+		else root->right = r;
+		return r;
+	}
+
+	if (data == r->data) { return root; }
+	else if (data > r->data)
+		CreateTree(r, r->left, data);
+	else
+		CreateTree(r, r->right, data);
+
+	return root;
+}
+
+void print_tree(struct Node* r, int l)
+{
+
+	if (r == NULL)
+	{
+		return;
+	}
+
+	print_tree(r->right, l + 1);
+	for (int i = 0; i < l; i++)
+	{
+		printf(" ");
+	}
+
+	printf("%d\n", r->data);
+	print_tree(r->left, l + 1);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int D, start = 1;
+
+	root = NULL;
+	printf("-1 - окончание построения дерева\n");
+	while (start)
+	{
+		printf("Введите число: ");
+		scanf_s("%d", &D);
+		if (D == -1)
+		{
+			printf("Построение дерева окончено\n\n");
+			start = 0;
+		}
+		else
+			root = CreateTree(root, root, D);
+
+	}
+
+	print_tree(root, 0);
+
+	return 0;
+}
